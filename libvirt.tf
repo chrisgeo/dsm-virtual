1c91b2a6ae75e7d38dc42bb48872ab12101a3bc6
Chris George <chrisgeo@users.noreply.github.com>
2025-06-16 02:07:26 +0000

    DSM Server

---
+++ libvirt.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">=1.10.0"
+
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = ">=0.6.2"
+    }
+  }
+}
+
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
